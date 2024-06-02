fee0840e812f9c6c812fa3a4d72006d19a415882
OleksandrBazhyn <olexandrbazhyn@gmail.com>
2024-06-02 22:48:24 +0300

    Added Controllers for Models

---
+++ UniversityAdmissionWEBAPI/Controllers/AdmissionRequestsController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniversityAdmissionWEBAPI.Models;
+
+namespace UniversityAdmissionWEBAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdmissionRequestsController : ControllerBase
+    {
+        private readonly UniversityAdmissionAPIContext _context;
+
+        public AdmissionRequestsController(UniversityAdmissionAPIContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/AdmissionRequests
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AdmissionRequest>>> GetAdmissionRequests()
+        {
+            return await _context.AdmissionRequests.ToListAsync();
+        }
+
+        // GET: api/AdmissionRequests/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AdmissionRequest>> GetAdmissionRequest(int id)
+        {
+            var admissionRequest = await _context.AdmissionRequests.FindAsync(id);
+
+            if (admissionRequest == null)
+            {
+                return NotFound();
+            }
+
+            return admissionRequest;
+        }
+
+        // PUT: api/AdmissionRequests/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutAdmissionRequest(int id, AdmissionRequest admissionRequest)
+        {
+            if (id != admissionRequest.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(admissionRequest).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!AdmissionRequestExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/AdmissionRequests
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<AdmissionRequest>> PostAdmissionRequest(AdmissionRequest admissionRequest)
+        {
+            _context.AdmissionRequests.Add(admissionRequest);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetAdmissionRequest", new { id = admissionRequest.Id }, admissionRequest);
+        }
+
+        // DELETE: api/AdmissionRequests/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAdmissionRequest(int id)
+        {
+            var admissionRequest = await _context.AdmissionRequests.FindAsync(id);
+            if (admissionRequest == null)
+            {
+                return NotFound();
+            }
+
+            _context.AdmissionRequests.Remove(admissionRequest);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AdmissionRequestExists(int id)
+        {
+            return _context.AdmissionRequests.Any(e => e.Id == id);
+        }
+    }
+}
